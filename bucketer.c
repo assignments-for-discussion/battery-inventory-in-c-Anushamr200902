21031eeeeb77d25af164502b782000337048d1a0
Anushamr200902 <144462691+Anushamr200902@users.noreply.github.com>
2023-09-08 19:52:28 +0530

    Update bucketer.c

--- bucketer.c
+++ bucketer.c
@@ -1,50 +1,49 @@
-import java.util.HashMap;
-import java.util.Map;
 
-public class BatteryClassification {
-    public static void main(String[] args) {
-        double ratedCapacity = 120.0; // Rated capacity of all batteries (in Ah)
-        double[] presentCapacities = {105.0, 95.0, 60.0, 110.0, 75.0, 100.0};
+#include <stdio.h>
 
-        Map<String, Integer> classificationCounts = countBatteryClassification(presentCapacities, ratedCapacity);
+// Function to calculate SoH (State of Health)
+double calculateSoH(double present_capacity, double rated_capacity) {
+    return (present_capacity / rated_capacity) * 100.0;
+}
 
-        System.out.println("Classification Counts:");
-        for (Map.Entry<String, Integer> entry : classificationCounts.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
-    }
-
-    public static Map<String, Integer> countBatteryClassification(double[] presentCapacities, double ratedCapacity) {
-        Map<String, Integer> classifications = new HashMap<>();
-        classifications.put("Healthy", 0);
-        classifications.put("Replace", 0);
-        classifications.put("Failed", 0);
-
-        for (double presentCapacity : presentCapacities) {
-            double soh = calculateSoH(presentCapacity, ratedCapacity);
-            String classification = classifyBattery(soh);
-            classifications.put(classification, classifications.get(classification) + 1);
-        }
-
-        return classifications;
-    }
-
-    public static double calculateSoH(double presentCapacity, double ratedCapacity) {
-        return (presentCapacity / ratedCapacity) * 100.0;
-    }
-
-    public static String classifyBattery(double soh) {
-        if (soh > 80.0) {
-            return "Healthy";
-        } else if (soh >= 65.0) {
-            return "Replace";
-        } else {
-            return "Failed";
-        }
+// Function to classify a battery based on SoH
+char* classifyBattery(double soh) {
+    if (soh > 80.0) {
+        return "Healthy";
+    } else if (soh >= 65.0) {
+        return "Replace";
+    } else {
+        return "Failed";
     }
 }
 
 int main() {
-  testBucketingByHealth();
-  return 0;
+    double rated_capacity = 120.0; // Rated capacity of all batteries (in Ah)
+    double present_capacities[] = {105.0, 95.0, 60.0, 110.0, 75.0, 100.0};
+    int num_batteries = sizeof(present_capacities) / sizeof(present_capacities[0]);
+
+    int healthy_count = 0;
+    int replace_count = 0;
+    int failed_count = 0;
+
+    // Loop through each battery, calculate SoH, classify, and count
+    for (int i = 0; i < num_batteries; i++) {
+        double soh = calculateSoH(present_capacities[i], rated_capacity);
+        char* classification = classifyBattery(soh);
+
+        if (classification == "Healthy") {
+            healthy_count++;
+        } else if (classification == "Replace") {
+            replace_count++;
+        } else {
+            failed_count++;
+        }
+    }
+
+    printf("Classification Counts:\n");
+    printf("Healthy: %d\n", healthy_count);
+    printf("Replace: %d\n", replace_count);
+    printf("Failed: %d\n", failed_count);
+
+    return 0;
 }
