f5dad0b88a06da990d5ae11b88758bb33a5aac27
Anushamr200902 <144462691+Anushamr200902@users.noreply.github.com>
2023-09-08 19:50:32 +0530

    Update bucketer.c

--- bucketer.c
+++ bucketer.c
@@ -1,26 +1,47 @@
-#include <stdio.h>
-#include <assert.h>
+import java.util.HashMap;
+import java.util.Map;
 
-struct CountsBySoH {
-  int healthy;
-  int exchange;
-  int failed;
-};
+public class BatteryClassification {
+    public static void main(String[] args) {
+        double ratedCapacity = 120.0; // Rated capacity of all batteries (in Ah)
+        double[] presentCapacities = {105.0, 95.0, 60.0, 110.0, 75.0, 100.0};
 
-struct CountsBySoH countBatteriesByHealth(const int* presentCapacities, int nBatteries) {
-  struct CountsBySoH counts = {0, 0, 0};
-  return counts;
-}
+        Map<String, Integer> classificationCounts = countBatteryClassification(presentCapacities, ratedCapacity);
 
-void testBucketingByHealth() {
-  const int presentCapacities[] = {115, 118, 80, 95, 91, 77};
-  const int numberOfBatteries = sizeof(presentCapacities) / sizeof(presentCapacities[0]);
-  printf("Counting batteries by SoH...\n");
-  struct CountsBySoH counts = countBatteriesByHealth(presentCapacities, numberOfBatteries);
-  assert(counts.healthy == 2);
-  assert(counts.exchange == 3);
-  assert(counts.failed == 1);
-  printf("Done counting :)\n");
+        System.out.println("Classification Counts:");
+        for (Map.Entry<String, Integer> entry : classificationCounts.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    public static Map<String, Integer> countBatteryClassification(double[] presentCapacities, double ratedCapacity) {
+        Map<String, Integer> classifications = new HashMap<>();
+        classifications.put("Healthy", 0);
+        classifications.put("Replace", 0);
+        classifications.put("Failed", 0);
+
+        for (double presentCapacity : presentCapacities) {
+            double soh = calculateSoH(presentCapacity, ratedCapacity);
+            String classification = classifyBattery(soh);
+            classifications.put(classification, classifications.get(classification) + 1);
+        }
+
+        return classifications;
+    }
+
+    public static double calculateSoH(double presentCapacity, double ratedCapacity) {
+        return (presentCapacity / ratedCapacity) * 100.0;
+    }
+
+    public static String classifyBattery(double soh) {
+        if (soh > 80.0) {
+            return "Healthy";
+        } else if (soh >= 65.0) {
+            return "Replace";
+        } else {
+            return "Failed";
+        }
+    }
 }
 
 int main() {
